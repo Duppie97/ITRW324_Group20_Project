d9c7d4e45a520f1174292e55de80ded077418115
DESKTOP-GR3CRQ0\Johannnnnn <25847465@student.g.nwu.ac.za>
2017-08-11 15:36:06 +0200

    Log in, sign up, api

---
+++ index.js
@@ -0,0 +1,104 @@
+var imgPath = "img/";
+var imgPath2 = "http://localhost/img/"
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+function changeImage(id) {
+        if (document.getElementById(id).src == imgPath2 + id + "-h.png") 
+        {
+            document.getElementById(id).src = imgPath + id + ".png";
+            document.getElementById(id).value = "true";
+        }
+        else 
+        {
+            document.getElementById(id).src = imgPath + id + "-h.png";
+            document.getElementById(id).value = "false";
+        }
+    }
+
+function hoverImage(id) {
+        if (document.getElementById(id).src == imgPath2 + id + "-g.png") 
+        {
+            document.getElementById(id).src = imgPath + id + "-h.png";
+        }
+        
+    }   
+
+function hoverOff(id) {
+
+        if (document.getElementById(id).src == imgPath2 + id + "-h.png") 
+        {
+            document.getElementById(id).src = imgPath + id + "-g.png";
+        }
+        
+    } 
+
+function validate()
+{
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("GET", "http://localhost/api.php", false);
+  alert("2");
+  xhttp.setRequestHeader("Content-type", "application/json");
+  alert("3");
+  xhttp.send("aaaaaa");
+  alert("4");
+  alert(xhttp.responseText);
+  var response = JSON.parse(xhttp.responseText);
+  
+}
+
+$( document ).ready(function() {
+  $( '.gallery' ).each( function ( ) {
+    var id = ($(this).attr('id'));
+      this.src=imgPath+id+"-g.png";
+      this.width=100;
+      this.style.paddingRight="10px";
+      this.style.paddingBottom="20px";
+      this.title = id;
+  });
+});
+
