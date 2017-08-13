fe90ccbf21ae8fea059da214f4177cd6c792296c
DevlinZA <jaco.vanzyl@creative-sound.net>
2017-08-13 22:53:57 +0200

    Active session se files

--- database_connection_functions.js
+++ database_connection_functions.js
@@ -7,7 +7,7 @@ function useDB()
   var key = "";
 
   //INSERT INTO table (column) VALUES (value)   -> format {'column1' : 'value1', 'column2' : 'value2'}
-  var myArr = {'column': 'value'};
+  var myArr = {'': ''};
 
   //SELECT columns FROM table                   -> format "&column1,column2,column3"
   var columns = "";
---
+++ session.js
@@ -0,0 +1,39 @@
+
+
+var connection = new RTCMultiConnection();
+
+window.onbeforeunload = function(e){
+hangup();
+}
+
+// this line is VERY_important
+connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+
+// all below lines are optional; however recommended.
+
+connection.session = {
+    audio: true,
+    video: true
+};
+
+
+
+connection.sdpConstraints.mandatory = {
+    OfferToReceiveAudio: false,
+    OfferToReceiveVideo: true
+};
+
+connection.onstream = function(event) {
+    document.body.appendChild( event.mediaElement );
+};
+
+var predefinedRoomId = prompt('Please enter room-id', 'xyzxyzxyz');
+
+connection.openOrJoin(predefinedRoomId);
+
+function joinScreen()
+{
+
+	window.location = 'localhost/session.html';
+}
+window.alert("sometext");
