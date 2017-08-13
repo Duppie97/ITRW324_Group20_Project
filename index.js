0b76a7d3d85ed0a5bdc471eb3d3b02fd99e6c789
Ruan du Plessis <ruan@ics.co.za>
2017-08-13 17:17:34 +0200

    Changes and insert Join Session

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+$('.form').ready(function() {
+    var table = $('#example').DataTable();
+ 
+    $('#example tbody').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    } );
+ 
+    $('#button').click( function () {
+        table.row('.selected').remove().draw( false );
+    } );
+} );
 var imgPath = "img/";
 var imgPath2 = "http://localhost/img/"
 
