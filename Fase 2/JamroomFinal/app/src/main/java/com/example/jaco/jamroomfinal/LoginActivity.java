d4c3ad2d4339fc63add02407846905ee33dd2167
24249270 Andries Bester <24249270>
2017-09-15 11:51:12 +0200

    added activities

---
+++ Fase 2/JamroomFinal/app/src/main/java/com/example/jaco/jamroomfinal/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.jaco.jamroomfinal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Map;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void gotoMainPage(View view)
+    {
+        Intent nextPage = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity((nextPage));
+    }
+
+
+}
